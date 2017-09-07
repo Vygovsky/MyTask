eca7554eebf134da3dacaff107df9c3ca1792dba
Roman_v <vigmol@ukr.net>
2017-09-07 22:50:29 +0300

    to fix

--- src/task1/SortAlphabetically.java
+++ src/task1/SortAlphabetically.java
@@ -1,20 +1,24 @@
 package task1;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class SortAlphabetically {
     public static void main(String[] args) {
-
+        Scanner sc2 = new Scanner(System.in);
+        getLetter2(sc2);
     }
 
     public void getLetter(Scanner sc) {
-        String s = sc.nextLine();
-        if (s.compareTo(sc.nextLine())<0) {
-            String t = s;
-
+        List<String> scr = new ArrayList<>();
+        String scanner = sc.nextLine();
+        if (scanner.compareTo(sc.nextLine()) < 0) {
 
 
         }
 
+    }
+    public static void getLetter2(Scanner sc){
+        Arrays.stream(new Scanner(System.in).nextLine().split("[\\s ]+")).
+        sorted(Comparator.comparing(String::toLowerCase)).forEach(System.out::println);
     }
 }
