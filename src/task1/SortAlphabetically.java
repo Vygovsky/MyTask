573810238ca03141baadbc56b47266fec38a1e05
roman_v <vigmol@ukr.net>
2017-09-07 15:31:48 +0300

    to fix task1 in work

---
+++ src/task1/SortAlphabetically.java
@@ -0,0 +1,20 @@
+package task1;
+
+import java.util.Scanner;
+
+public class SortAlphabetically {
+    public static void main(String[] args) {
+
+    }
+
+    public void getLetter(Scanner sc) {
+        String s = sc.nextLine();
+        if (s.compareTo(sc.nextLine())<0) {
+            String t = s;
+
+
+
+        }
+
+    }
+}
