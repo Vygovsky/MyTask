ba3e3de27738a78bdf97de39934189f81beeb667
roman_v <vigmol@ukr.net>
2017-09-08 13:58:41 +0300

    to fix task1

--- src/task1/SortAlphabetically.java
+++ src/task1/SortAlphabetically.java
@@ -5,16 +5,23 @@ import java.util.*;
 public class SortAlphabetically {
     public static void main(String[] args) {
         Scanner sc2 = new Scanner(System.in);
-        getLetter2(sc2);
+       getLetter2(sc2);
+        //getLetter(sc2);
+
     }
 
-    public void getLetter(Scanner sc) {
-        List<String> scr = new ArrayList<>();
-        String[] scanner = sc.nextLine().split("[\\s ]+ ");
-        if (sc.nextLine().compareTo(scanner))<0){
+    public static void getLetter(Scanner sc) {
+        String[] words = sc.nextLine().split("[\\s ]+ ");
+        for (int i = 0; i < words.length; i++)
+            for (int j = words.length - 1; j > i; j++)
+                if (words[0].length() > words[1].length()) {
+                String temp=words[0];
+                words[0]=words[1];
+                words[1]=temp;
 
 
-        }
+
+                }
 
     }
 
