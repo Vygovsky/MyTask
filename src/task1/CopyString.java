92d89f93d75f5cad91e228bec5a11a9044b86539
roman_v <vigmol@ukr.net>
2017-09-13 15:03:36 +0300

    to fix task1

--- src/task1/CopyString.java
+++ src/task1/CopyString.java
@@ -1,20 +1,31 @@
 package task1;
 
+import java.util.Arrays;
+
 public class CopyString {
     public static void main(String[] args) {
         String[] s = {"a", "bb", "ccc"};
 
         for (String word : s) {
             System.out.println(word);
-
-            System.out.println("-----------------------");
+            getTolk(new String[]{"oo"});
 
             StringBuilder sb = new StringBuilder(word);
-            sb.append("aa");
-            sb.append("bbbb");
-            sb.append("ccccccccc");
-
+            for (int i = 1; i < 3; i++) {
+                sb.append(word);
+            }
             System.out.println(sb);
+            String[] copyS = Arrays.copyOf(s, word.length());
+            System.out.println(copyS.toString());
+        }
+
+    }
+
+    public static void getTolk(String[] words) {
+        String[] strArrey = new String[3];
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < strArrey.length; i++) {
+            sb.append(strArrey[i]);
         }
     }
 }
