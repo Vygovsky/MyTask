e94191c1650df2cac2cd8b60ec7f121e5a6a938e
roman_v <vigmol@ukr.net>
2017-09-13 11:56:39 +0300

    to fix task1

--- src/task1/SortAlphabetically.java
+++ src/task1/SortAlphabetically.java
@@ -79,17 +79,17 @@ public class SortAlphabetically {
         }
         for (int i = 0; i < list.size(); i++) {
             for (int j = list.size() - 1; j > i; j--) {
-                String firstStr = list.get(j);
-                int firstStrLength = firstStr.length();
-                int indexOfFirstSpaceOfFirstStr = firstStr.indexOf(' ');
-                String subFirstStr = firstStr.substring(indexOfFirstSpaceOfFirstStr+1, firstStrLength-1);
+   //             String firstStr = list.get(j);
+     //           int firstStrLength = firstStr.length();
+       //         int indexOfFirstSpaceOfFirstStr = firstStr.indexOf(' ');
+         //       String subFirstStr = firstStr.substring(indexOfFirstSpaceOfFirstStr+1, firstStrLength-1);
 
                 String secondStr = list.get(j-1);
                 int secondStrLength = secondStr.length();
-                int indexOfFirstSpaceOfSecondStr = firstStr.indexOf(' ');
-                String subSecondStr = firstStr.substring(indexOfFirstSpaceOfSecondStr+1, secondStrLength-1);
+                int indexOfFirstSpaceOfSecondStr = secondStr.indexOf(' ');
+                String subSecondStr = secondStr.substring(indexOfFirstSpaceOfSecondStr+1, secondStrLength-1);
 
-                if (subFirstStr.compareTo(subSecondStr) < 0) {
+                if (secondStr.compareTo(subSecondStr) < 0) {
                     String remove = list.remove(j - 1);
                     list.add(j, remove);
                 }
