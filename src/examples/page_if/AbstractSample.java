8d0ea88cea836475e800c195a4497ead72448ecd
roman_v <vigmol@ukr.net>
2017-09-18 11:02:03 +0300

    to fix pageif

---
+++ src/examples/page_if/AbstractSample.java
@@ -0,0 +1,10 @@
+package examples.page_if;
+
+public class AbstractSample {
+    public static int anInt(){
+
+     int result= (int) Math.random(anInt()*10);
+        System.out.println("Int value= "+result);
+        return result;
+    }
+}
