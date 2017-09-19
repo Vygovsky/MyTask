a4b0894e7a3663127fecff00fb53de71eb62024a
Roman_v <vigmol@ukr.net>
2017-09-19 22:48:52 +0300

    to fix task1

--- src/task1/StringLastIndex.java
+++ src/task1/StringLastIndex.java
@@ -1,6 +1,6 @@
 package task1;
 
-import org.junit.Test;
+
 
 public class StringLastIndex {
     public static void main(String[] args) {
@@ -8,8 +8,10 @@ public class StringLastIndex {
 
         for (int i = 0; i < a.length; i++) {
             String s = String.valueOf(a[i]);
-            char ch = (char) s.lastIndexOf(1);
-            System.out.println(ch);
+            char ch = (char) (s.length()-1);
+            int r=Integer.valueOf(ch);
+            System.out.println(r);
+
 
         }
 
