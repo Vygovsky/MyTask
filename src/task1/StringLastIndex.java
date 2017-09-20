e7987de1000b8cb85e6a10fb0417331e1580a1bc
roman_v <vigmol@ukr.net>
2017-09-20 14:48:21 +0300

    to fix task1

--- src/task1/StringLastIndex.java
+++ src/task1/StringLastIndex.java
@@ -1,20 +1,19 @@
 package task1;
 
 
+import java.util.Arrays;
 
 public class StringLastIndex {
     public static void main(String[] args) {
         int[] a = {1, 22, 3366, 99};
+        int[] b;
 
         for (int i = 0; i < a.length; i++) {
             String s = String.valueOf(a[i]);
-            char ch = (char) (s.length()-1);
-            int r=Integer.valueOf(ch);
-            System.out.println(r);
-
-
+            int res = Integer.parseInt(s.substring(s.length() - 1));
+            a[i]=res;
+            System.out.println(a[i]);
         }
-
     }
 }
 
