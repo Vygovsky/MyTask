9e8dd9c4f540446a22169574538de37ba7366b0d
roman_v <vigmol@ukr.net>
2017-09-21 15:47:48 +0300

    to fix task1

--- src/task1/RemoveSubString.java
+++ src/task1/RemoveSubString.java
@@ -1,18 +1,20 @@
 package task1;
 
-import org.junit.Test;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 
 public class RemoveSubString {
     public static void main(String[] args) {
         String regex = "Hello there";
-        //StringBuilder sb = new StringBuilder(s);
+        StringBuilder sb = new StringBuilder(regex);
         Pattern pattern= Pattern.compile("ll");
-        Matcher matcher=pattern.matcher(regex);
+        Matcher matcher=pattern.matcher(sb);
         while (matcher.find()){
-           regex.substring(matcher.)
+            System.out.println(sb.delete(2,3));
+
+
+
         }
 
 
