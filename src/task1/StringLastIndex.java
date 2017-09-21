84f86870a02c2a34418c3c15adafd7ae9021aa76
roman_v <vigmol@ukr.net>
2017-09-21 08:27:30 +0300

    to fix task1

--- src/task1/StringLastIndex.java
+++ src/task1/StringLastIndex.java
@@ -5,13 +5,22 @@ import java.util.Arrays;
 
 public class StringLastIndex {
     public static void main(String[] args) {
-        int[] a = {1, 22, 3366, 99};
-      // getlast(a);
+        int[] a = {1, 22, 3396, 99, 180, 2963, 777};
+        getLastIndex(a);
+        System.out.println();
+        getLastIndex2(a);
 
 
+        }
+
+    public static int[] getLastIndex2(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            a[i] = a[i] % 10;
+            System.out.print("[" + a[i] + "" + "]");
+        }
+        return a;
     }
-
-    public static int[] getlast(int[] a) {
+    public static int[] getLastIndex(int[] a) {
         for (int i = 0; i < a.length; i++) {
             String s = String.valueOf(a[i]);
             int res = Integer.parseInt(s.substring(s.length() - 1));
