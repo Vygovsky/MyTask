e591da1998831af66c4a69f1eaa350627cecefee
roman_v <vigmol@ukr.net>
2017-09-21 15:12:31 +0300

    to fix task1

---
+++ src/task1/RemoveSubString.java
@@ -0,0 +1,20 @@
+package task1;
+
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RemoveSubString {
+    public static void main(String[] args) {
+        String regex = "Hello there";
+        //StringBuilder sb = new StringBuilder(s);
+        Pattern pattern= Pattern.compile("ll");
+        Matcher matcher=pattern.matcher(regex);
+        while (matcher.find()){
+           regex.substring(matcher.)
+        }
+
+
+    }
+}
