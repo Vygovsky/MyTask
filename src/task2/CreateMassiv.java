9ebd8e405fd6e3cc894e4fe633a65d675b6118b7
roman_v <vigmol@ukr.net>
2017-10-02 16:04:11 +0300

    to fix task2

--- src/task2/CreateMassiv.java
+++ src/task2/CreateMassiv.java
@@ -1,13 +1,42 @@
 package task2;
 
+import java.util.Collections;
+
 public class CreateMassiv {
     public static void main(String[] args) {
         int[] array = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        getProba(array);
+        System.out.println("--------------------------------------");
+
+
+        int[] array2 = new int[100];
+        for (int i = 0; i < 100; i++) {
+            array2[i]=i;
+            if (i % 2 != 0) {
+                System.out.print(array2[i] + " ");
+
+                }
+                for (int j =array2.length-1 ; j >0; j--) {
+                    System.out.println(array2[i]);
+            }
+        }
+
+        //        array2[i]=i;
+        //      if (array2[i] % 2 == 0) {
+        //          System.out.print(array2[i]+" ");
+
+        //      }
+
+    }
+
+
+    public static int[] getProba(int[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
-        for (int i = 0; i <array.length ; i++) {
-            System.out.println(array[i]+" ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + " ");
         }
+        return array;
     }
 }
