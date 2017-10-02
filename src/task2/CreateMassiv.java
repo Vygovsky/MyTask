7f2316d083c535bf15e165d1cad0781ee3367f8c
roman_v <vigmol@ukr.net>
2017-10-02 13:08:35 +0300

    to fix task2

---
+++ src/task2/CreateMassiv.java
@@ -0,0 +1,16 @@
+package task2;
+
+public class CreateMassiv {
+    public static void main(String[] args) {
+        int[] array = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+
+
+        }
+        for (int i = 0; i <array.length ; i++) {
+            System.out.println(array[i]+" ");
+        }
+
+    }
+}
