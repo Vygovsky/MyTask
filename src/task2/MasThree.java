66319d9df2a61a63eeabfdcb05a34a7577392213
roman_v <vigmol@ukr.net>
2017-10-04 10:36:20 +0300

    to fix task2

---
+++ src/task2/MasThree.java
@@ -0,0 +1,17 @@
+package task2;
+
+public class MasThree {
+    public static void main(String[] args) {
+        int[] array3 = new int[10];
+
+        for (int i = 0; i < array3.length; i++) {
+            array3[i] = ((int) Math.round(Math.random() * 10));
+            System.out.print(array3[i] + " ");
+            if (array3[i] % 2 != 0) {
+                array3[i] = 0;
+            }
+            System.out.print(" ");
+            System.out.println(array3[i]);
+        }
+    }
+}
--- src/task2/MassTwo.java
+++ src/task2/MassTwo.java
@@ -1,20 +1,29 @@
 package task2;
 
+import java.util.Random;
+
 public class MassTwo {
     public static void main(String[] args) {
         int[] array2 = new int[15];
         int index = 0;
-        for (int i = 0; i < array2.length; i++) {
-            array2[i] = (int) Math.round(Math.random() * 9);
+       for (int i = 0; i < array2.length; i++) {
+/*            array2[i] = (int) Math.round(Math.random() * 9);
             System.out.print(array2[i] + " ");
 
-            if (array2[i] % 2 == 0) {
-                array2[index] = i;
-                System.out.println(" ");
-                System.out.println(index + " ");
+            if (array2[i] > 0 & array2[i] % 2 == 0) {
+                index++;
             }
-
-
-        }
+            System.out.println(" ");
+            System.out.println("Summa chetnih chisel: " );
+        }*/
+         Random ram = new Random();
+           array2[i] = ram.nextInt(9);
+           System.out.print(array2[i] + " ");
+           if (array2[i] % 2 == 0)
+               index++;
+           }
+           System.out.println(" ");
+           System.out.println("Всего в массиве "+index+" четнных чисел!");
+       }
     }
-}
+
