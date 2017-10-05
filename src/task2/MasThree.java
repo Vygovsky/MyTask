a056f01884d500c24f6a930afd3f5dce52923364
roman_v <vigmol@ukr.net>
2017-10-05 09:17:58 +0300

    to fix task2

--- src/task2/MasThree.java
+++ src/task2/MasThree.java
@@ -5,7 +5,7 @@ public class MasThree {
         int[] array3 = new int[10];
 
         for (int i = 0; i < array3.length; i++) {
-            array3[i] = ((int) Math.round(Math.random() * 10)+1);
+            array3[i] = ((int) Math.round(Math.random() * 9)+1);
             System.out.print(array3[i] + " ");
             if (array3[i] % 2 != 0) {
                 array3[i] = 0;
--- src/task2/MassFive.java
+++ src/task2/MassFive.java
@@ -5,7 +5,11 @@ public class MassFive {
         int[] array5 = new int[4];
         for (int i = 0; i < array5.length; i++) {
             array5[i] = (int) Math.round(Math.random() * 90)+10;
-            System.out.println(array5[i]+" ");
+            System.out.print(array5[i]+" ");
+            if(array5[i]-1>array5[i]){
+
+
+            }
         }
     }
 }
