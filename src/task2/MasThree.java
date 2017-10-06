79e824df6305ae7841841bf0f4a2758f43f5614e
Petrykin Sergey <serhii.petrykin@gmail.com>
2017-10-06 10:23:15 +0300

    Update MasThree.java
    Some improvements :)

--- src/task2/MasThree.java
+++ src/task2/MasThree.java
@@ -2,16 +2,15 @@ package task2;
 
 public class MasThree {
     public static void main(String[] args) {
-        int[] array3 = new int[10];
-
+        int[] array3 = new int[15];
+        int countOfOddElement = 0;
         for (int i = 0; i < array3.length; i++) {
-            array3[i] = ((int) Math.round(Math.random() * 9)+1);
+            array3[i] = (int) Math.round(Math.random() * 9);
             System.out.print(array3[i] + " ");
             if (array3[i] % 2 != 0) {
-                array3[i] = 0;
+                countOfOddElement++;
             }
-            System.out.print(" ");
-            System.out.println(array3[i]);
         }
+        System.out.print("\nCount of odd elements: " + countOfOddElement);
     }
 }
