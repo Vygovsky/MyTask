18c550bdec5c767c9a9153a50a1cfaa99d8ff609
roman_v <vigmol@ukr.net>
2017-10-06 16:01:09 +0300

    to fix task2

--- src/task2/MassFoure.java
+++ src/task2/MassFoure.java
@@ -5,27 +5,29 @@ import java.util.Random;
 public class MassFoure {
     public static void main(String[] args) {
         Random ram = new Random();
-        int[] array1 = new int[6];
-        int[] array2 = new int[6];
-        int sum1 = 0;
-        int sum2 = 0;
+        int[] array1 = new int[5];
+        int[] array2 = new int[5];
+        double average1 = 0;
+        double average2 = 0;
+
         for (int i = 0; i < array1.length; i++) {
             array1[i] = ram.nextInt(5);
             System.out.print(array1[i] + " ");
-            sum1 = sum1 + array1[i];
+            average1 = (average1 + array1[i]) / array1.length;
+
         }
-        System.out.println("=> Sum 1 = " + sum1);
+        System.out.println("=> Среднее арифметическое значение  = " + average1);
 
         for (int i = 0; i < array2.length; i++) {
             array2[i] = ram.nextInt(5);
             System.out.print(array2[i] + " ");
-            sum2 = sum2 + array2[i];
+            average2 = (average2 + array1[i]) / array1.length;
         }
-        System.out.println("=> Sum 2 = " + sum2);
+        System.out.println("=> Среднее арифметическое значение  = " + average2);
 
-        if (sum1 > sum2) {
+        if (average1 > average2) {
             System.out.println("Первый массив больше чем второй");
-        } else if (sum1 < sum2) {
+        } else if (average1 < average2) {
             System.out.println("Второй массив больше чем первый ");
         } else {
             System.out.println("Массивы равны ");
