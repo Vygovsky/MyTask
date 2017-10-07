8d76ce87f75887c997b712aaeaaf50a3ae04bb63
Roman_v <vigmol@ukr.net>
2017-10-07 20:47:30 +0300

    to fix task10

--- src/task2/task10/MassDemo10.java
+++ src/task2/task10/MassDemo10.java
@@ -7,13 +7,34 @@ import java.util.Random;
  */
 public class MassDemo10 {
     public static void main(String[] args) {
-        int[] array=new int[11];
-        Random random=new Random();
-        for (int i = 0; i <array.length ; i++) {
-            array[i]=random.nextInt(3)-1;
-            System.out.print(array[i]+" ");
+        int[] array = new int[11];
+        int minusOne = 0;
+        int zero = 0;
+        int plusOne = 0;
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(3) - 1;
+            System.out.print(array[i] + "; ");
         }
-        for (int i = 0; i <array.length ; i++) {
+        System.out.println();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == -1) {
+                minusOne++;
+            }
+            if (array[i] == 0) {
+                zero++;
+            }
+            if (array[i] == 1) {
+                plusOne++;
+            }
+        }
+
+        if (minusOne > zero & minusOne > plusOne) {
+            System.out.println("Элемент -1 встречается в массиве чаще всего = " + minusOne);
+        } else if (zero > minusOne && zero > plusOne) {
+            System.out.println("Элемент 0 встречается в массиве чаще всего = " + zero);
+        } else {
+            System.out.println("Элемент 1 встречается в массиве чаще всего = " + plusOne);
 
         }
     }
