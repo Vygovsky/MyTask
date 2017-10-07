c1599048d8963a930e8fa7f83623428ecb0885ec
Roman_v <vigmol@ukr.net>
2017-10-07 17:45:17 +0300

    to fix task8

---
+++ src/task2/task10/MassDemo10.java
@@ -0,0 +1,22 @@
+package task2.task10;
+
+import java.util.Random;
+
+/**
+ * Created by Roman_v on 07.10.2017.
+ */
+public class MassDemo10 {
+    public static void main(String[] args) {
+        int[] array=new int[11];
+
+        Random random=new Random();
+        for (int i = 0; i <array.length ; i++) {
+            array[i]=random.nextInt(3)-1;
+            System.out.print(array[i]+" ");
+        }
+        for (int i = 0; i <array.length ; i++) {
+
+        }
+    }
+}
+
