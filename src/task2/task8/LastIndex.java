25d8cadc2dde7187cd62f51bf23e9ef632ea4522
Roman_v <vigmol@ukr.net>
2017-10-07 17:04:30 +0300

    to fix task8

---
+++ src/task2/task8/LastIndex.java
@@ -0,0 +1,32 @@
+package task2.task8;
+
+import java.util.Random;
+
+/**
+ * Created by Roman_v on 07.10.2017.
+ */
+public class LastIndex {
+    public static void main(String[] args) {
+        int[] array8 = new int[31];
+        getLastIndex(array8);
+    }
+
+    public static void getLastIndex(int[]array8) {
+        int index = 0;
+        int max = 0;
+        Random random = new Random();
+        for (int i = 0; i < array8.length; i++) {
+            array8[i] = random.nextInt(31) - 15;
+            System.out.print(array8[i] + "; ");
+        }
+        for (int i = 0; i < array8.length; i++) {
+            if (max <= array8[i]) {
+                max = array8[i];
+                i = index++;
+            }
+        }
+        System.out.println();
+        System.out.println("Максимальное число: " + max + " номер по индексу равен = " + index);
+    }
+}
+
