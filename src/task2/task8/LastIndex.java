9ead9680c98bca9f87817462ad673269775fc71c
Petrykin Sergey <serhii.petrykin@gmail.com>
2017-10-10 10:19:20 +0300

    Update LastIndex.java

--- src/task2/task8/LastIndex.java
+++ src/task2/task8/LastIndex.java
@@ -12,22 +12,21 @@ public class LastIndex {
     }
 
     public static void getLastIndex(int[] array8) {
-        int index = 0;
-        int max = array8[0];
         Random random = new Random();
         for (int i = 0; i < array8.length; i++) {
             array8[i] = random.nextInt(31) - 15;
             System.out.print(array8[i] + "; ");
         }
-
+        int index = 0; // это тоже сюда перенес, чтоб все вместе было хотя можно было оставить и там
+        int max = array8[index]; // нужно писать здесь, потому как до первого цикла в array8[0] лежал 0
         for (int i = 0; i < array8.length; i++) {
             if (max <= array8[i]) {
                 max = array8[i];
-                index++;
+                index = i; // не index++, а именно index = i
             }
         }
         System.out.println();
-        System.out.println("Максимальное число: " + max + " номер по индексу равен = " + index);
+        System.out.println("Максимальное число: " + max + " индекс равен = " + index);
     }
 }
 
