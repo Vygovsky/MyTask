c2c419f5e4f7b0eaa4d27db13fb6e9d0a090073b
Petrykin Sergey <serhii.petrykin@gmail.com>
2017-10-10 15:00:09 +0300

    Update ThreeMas.java

--- src/task2/task9/ThreeMas.java
+++ src/task2/task9/ThreeMas.java
@@ -19,3 +19,21 @@ public class ThreeMas {
         }*/
     }
 }
+
+class ThreeMasSecondImplementation {
+    public static final int ARRAY_LENGTH = 10;
+    public static void main(String[] args) {
+        char[] arrChar1 = new char[ARRAY_LENGTH];
+        char[] arrChar2 = new char[ARRAY_LENGTH];
+        int[] arrInt = new int[ARRAY_LENGTH];
+        Random random = new Random();
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            arrChar1[i] = (char) random.nextInt(255);
+            arrChar2[i] = (char) random.nextInt(255);
+            arrInt[i] = arrChar1[i] + arrChar2[i];
+        }
+        System.out.println(Arrays.toString(arrChar1));
+        System.out.println(Arrays.toString(arrChar2));
+        System.out.println(Arrays.toString(arrInt));
+    }
+}
