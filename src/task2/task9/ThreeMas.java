6610e53274a6f957ba52cb6f23aa760d1cf98459
roman_v <vigmol@ukr.net>
2017-10-10 16:02:39 +0300

    to fix task9

--- src/task2/task9/ThreeMas.java
+++ src/task2/task9/ThreeMas.java
@@ -1,39 +1,43 @@
 package task2.task9;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class ThreeMas {
     public static void main(String[] args) {
-        int[] array = new int[10];
-        int[] array2 = new int[10];
-        double[] doubles = new double[10];
+        int array = 10;
+        int[] array1 = new int[array];
+        int[] array2 = new int[array];
+        double[] doubles = new double[array];
 
         Random random = new Random();
-        for (int i = 1; i < array.length; i++) {
-            array[i] = random.nextInt(9) + 1;
-            System.out.print(array[i] + " ");
+        for (int i = 0; i < array; i++) {
+            array1[i] = random.nextInt(9) + 1;
+            array2[i] = random.nextInt(9) + 1;
+            doubles[i] = (double) array1[i] / array2[i];
         }
-     /*   for (int i = 0; i < array.length; i++) {
-            doubles[i] = array[i] / array2[i];
-            System.out.println(doubles + " ");
-        }*/
+        System.out.println(Arrays.toString(array1));
+        System.out.println(Arrays.toString(array2));
+        System.out.println(Arrays.toString(doubles));
     }
 }
 
-class ThreeMasSecondImplementation {
-    public static final int ARRAY_LENGTH = 10;
-    public static void main(String[] args) {
-        char[] arrChar1 = new char[ARRAY_LENGTH];
-        char[] arrChar2 = new char[ARRAY_LENGTH];
-        int[] arrInt = new int[ARRAY_LENGTH];
-        Random random = new Random();
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
-            arrChar1[i] = (char) random.nextInt(255);
-            arrChar2[i] = (char) random.nextInt(255);
-            arrInt[i] = arrChar1[i] + arrChar2[i];
-        }
-        System.out.println(Arrays.toString(arrChar1));
+    /*class ThreeMasSecondImplementation {
+        public static final int ARRAY_LENGTH = 10;
+
+        public static void main(String[] args) {
+            char[] arrChar1 = new char[ARRAY_LENGTH];
+            char[] arrChar2 = new char[ARRAY_LENGTH];
+            int[] arrInt = new int[ARRAY_LENGTH];
+            Random random = new Random();
+            for (int i = 0; i < ARRAY_LENGTH; i++) {
+                arrChar1[i] = (char) random.nextInt(255);
+                arrChar2[i] = (char) random.nextInt(255);
+                arrInt[i] = arrChar1[i] + arrChar2[i];
+            }
+       System.out.println(Arrays.toString(arrChar1));
         System.out.println(Arrays.toString(arrChar2));
         System.out.println(Arrays.toString(arrInt));
+        }
     }
-}
+*/
