47fa1ab3e6c500f049a6f114570d5431ee173c2e
roman_v <vigmol@ukr.net>
2017-10-13 15:00:43 +0300

    to fix task10

--- src/task2/task10/MassDemo10.java
+++ src/task2/task10/MassDemo10.java
@@ -17,6 +17,15 @@ public class MassDemo10 {
             System.out.print(array[i] + "; ");
         }
         System.out.println();
+        ifOperator(array);
+
+       // getSwitch(array);
+    }
+
+    public static void ifOperator(int[] array) {
+        int minusOne = 0;
+        int zero = 0;
+        int plusOne = 0;
         for (int i = 0; i < array.length; i++) {
             if (array[i] == -1) {
                 minusOne++;
@@ -28,15 +37,44 @@ public class MassDemo10 {
                 plusOne++;
             }
         }
-
         if (minusOne > zero && minusOne > plusOne) {
             System.out.println("Элемент -1 встречается в массиве чаще всего = " + minusOne);
         } else if (zero > minusOne && zero > plusOne) {
             System.out.println("Элемент 0 встречается в массиве чаще всего = " + zero);
         } else {
             System.out.println("Элемент 1 встречается в массиве чаще всего = " + plusOne);
-
         }
     }
-}
 
+    public static void getSwitch(int[] array) {
+        int minIndex = 0;
+        int zero = 0;
+        int maxIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            switch (array[i]) {
+                case -1:
+                    minIndex++;
+                    break;
+                case 0:
+                    zero++;
+                    break;
+                case 1:
+                    maxIndex++;
+                    break;
+                default:
+                    System.out.println();
+                    break;
+            }
+        }
+        if (minIndex > zero && minIndex > maxIndex) {
+            System.out.println("Элемент -1 встречается в массиве чаще всего = " + minIndex);
+        } else if (zero > minIndex && zero > maxIndex) {
+            System.out.println("Элемент 0 встречается в массиве чаще всего = " + zero);
+        } else {
+            System.out.println("Элемент 1 встречается в массиве чаще всего = " + maxIndex);
+        }
+    }
+    public static void ternarOerator(){
+
+    }
+}
