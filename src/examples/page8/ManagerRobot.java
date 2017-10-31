a798b144b5cbfa4edb583921aa2a923abf1e70fb
roman_v <vigmol@ukr.net>
2017-10-31 15:59:36 +0200

    page8

--- src/examples/page8/ManagerRobot.java
+++ src/examples/page8/ManagerRobot.java
@@ -2,9 +2,20 @@ package examples.page8;
 
 public class ManagerRobot {
     public static void main(String[] args) {
-        RobotExt robot = new RobotExt(0,0,0);
+      RobotExt robot = new RobotExt(0,0,0);
 
-        robot.forward(20);
+      Robot robot1=new RobotTotal(0,0);
+
+
+      robot1.forward(20);
+      robot1.setCourse(90);
+      robot1.forward(45);
+      robot1.setCourse(50);
+      robot1.forward(10);
+      robot1.getCoordinate();
+
+
+        /*robot.forward(20);
         robot.getCoordinate();
 
         robot.setCourse(90);
@@ -16,6 +27,6 @@ public class ManagerRobot {
         robot.getCoordinate();
 
         robot.moveBack(10);
-        robot.getCoordinate();
+        robot.getCoordinate();*/
     }
 }
