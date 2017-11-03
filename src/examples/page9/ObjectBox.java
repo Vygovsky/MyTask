d5aa6a0c980057cbee3e083248d0b9aa46326627
roman_v <vigmol@ukr.net>
2017-11-03 15:31:33 +0200

    page9

---
+++ src/examples/page9/ObjectBox.java
@@ -0,0 +1,24 @@
+/*
+package examples.page9;
+
+class ObjectBox {
+    private Object object;
+    private ObjectBox next;
+
+    public Object getObject() {
+        return object;
+    }
+
+    public ObjectBox getNext() {
+        return next;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+
+    public void setNext(ObjectBox next) {
+        this.next = next;
+    }
+}
+*/
---
+++ src/examples/page9/ObjectQueue.java
@@ -0,0 +1,72 @@
+package examples.page9;
+
+public class ObjectQueue {
+    private ObjectBox head = null;
+    private ObjectBox tail = null;
+    private int size = 0;
+
+    public void push(Object obj) {
+        ObjectBox ob = new ObjectBox();
+        ob.setObject(obj);
+        if (head == null) {
+            head = ob;
+        } else {
+            tail.setNext(ob);
+        }
+        tail = ob;
+        size++;
+    }
+
+    public Object pull() {
+        if (size == 0) {
+            return null;
+        }
+        Object obj = head.getObject();
+        head = head.getNext();
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return obj;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Object get(int index) {
+        if (size == 0 || index >= size || index < 0) {
+            return null;
+        }
+        ObjectBox current = head;
+        int pos = 0;
+        if (pos < index) {
+            current = current.getNext();
+            pos++;
+        }
+        Object obj = current.getObject();
+        return obj;
+    }
+
+    private class ObjectBox {
+        private Object object;
+        private ObjectBox next;
+
+        public Object getObject() {
+            return object;
+        }
+
+        public ObjectBox getNext() {
+            return next;
+        }
+
+        public void setObject(Object object) {
+            this.object = object;
+        }
+
+        public void setNext(ObjectBox next) {
+            this.next = next;
+        }
+    }
+
+}
