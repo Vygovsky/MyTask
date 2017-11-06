1ad0605729696a7b0dbf03d6401de1f72e317593
roman_v <vigmol@ukr.net>
2017-11-06 16:16:07 +0200

    page8

--- src/examples/page8/ManagerRobot.java
+++ src/examples/page8/ManagerRobot.java
@@ -18,15 +18,47 @@ public class ManagerRobot {
 
         robot.moveBack(10);
         robot.getCoordinate();*/
-        final int COUNT = 10;
-        final int SIDE = 100;
-        Robot robot=new Robot(200,50);
-        for (int i = 0; i <COUNT ; i++) {
+      /*  final int COUNT = 10;
+        final int SIDE = 120;*/
+      int COUNT =3;
+      final int STEP = 30;
+        final int SiDE = 400;
+        int current=20;
+
+
+
+        Robot robot = new Robot(200, 200);
+        for (int i = 0; i < current; i++){
+            robot.forward(SiDE-i*10);
+            robot.setCourse(robot.getCourse()+300/COUNT);
+        }
+
+      /*  for (int i = 0; i < COUNT; i++) {
+            robot.setCourse(robot.getCourse() + 360 / (COUNT / 2));
             robot.forward(SIDE);
-            robot.setCourse(robot.getCourse()+360/COUNT);
-            RobotFrame rf=new RobotFrame(robot);
+            robot.setCourse(robot.getCourse() + 180 - 360 / COUNT);
+*/
+            RobotFrame rf = new RobotFrame(robot);
             rf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             rf.setVisible(true);
         }
+
     }
-}
+
+/* final int STEP = 30;
+        final int SiDE = 450;
+        int current = SiDE;*/
+ /*robot.forward(current);
+         robot.setCourse(robot.getCourse()+90);
+         while (current > 0) {
+         robot.forward(current);
+         robot.setCourse(robot.getCourse()+90);
+         robot.forward(current);
+         robot.setCourse(robot.getCourse()+90);
+         current-=STEP;
+
+         }
+         RobotFrame rf=new RobotFrame(robot);
+         rf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         rf.setVisible(true);
+*/
