7275f3dfca80d203ec4cfa28744443005a20fb0f
roman_v <vigmol@ukr.net>
2017-11-08 16:11:04 +0200

    page8

--- src/examples/page8/ManagerRobot.java
+++ src/examples/page8/ManagerRobot.java
@@ -1,13 +1,23 @@
 package examples.page8;
 
 import javax.swing.*;
+import java.util.Arrays;
 
 public class ManagerRobot {
     public static void main(String[] args) {
-       /* RobotExt robot = new RobotExt(0, 0, 0);
+        Robot[] rb = new Robot[10];
+        for (int i = 0; i < rb.length; i++) {
+            rb[i] = new Robot(i * 10, i * 10);
+        }
+        for (int i = 0; i < rb.length; i++) {
+            rb[i].getCoordinate();
+        }
 
-        Robot robot1 = new RobotTotal(0, 0);
 
+/*
+
+        RobotExt robot = new RobotExt(0, 0, 0);
+        Robot robot1 = new RobotTotal(10, 20);
 
         robot1.forward(20);
         robot1.setCourse(90);
@@ -17,34 +27,13 @@ public class ManagerRobot {
         robot1.getCoordinate();
 
         robot.moveBack(10);
-        robot.getCoordinate();*/
-      /*  final int COUNT = 10;
-        final int SIDE = 120;*/
-      int COUNT =3;
-      final int STEP = 30;
-        final int SiDE = 400;
-        int current=20;
-
-
-
-        Robot robot = new Robot(200, 200);
-        for (int i = 0; i < current; i++){
-            robot.forward(SiDE-i*10);
-            robot.setCourse(robot.getCourse()+300/COUNT);
-        }
-
-      /*  for (int i = 0; i < COUNT; i++) {
-            robot.setCourse(robot.getCourse() + 360 / (COUNT / 2));
-            robot.forward(SIDE);
-            robot.setCourse(robot.getCourse() + 180 - 360 / COUNT);
+        robot.getCoordinate();
 */
-            RobotFrame rf = new RobotFrame(robot);
-            rf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            rf.setVisible(true);
-        }
 
     }
 
+}
+//FIGURE
 /* final int STEP = 30;
         final int SiDE = 450;
         int current = SiDE;*/
@@ -62,3 +51,21 @@ public class ManagerRobot {
          rf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
          rf.setVisible(true);
 */
+  /*int COUNT = 3;
+  final int COUNT = 10;
+        final int SIDE = 120;
+        final int STEP = 30;
+        final int SiDE = 400;
+        int current = 20;
+
+
+        Robot robot = new Robot(200, 200);
+        for (int i = 0; i < current; i++){
+            robot.forward(SiDE-i*10);
+            robot.setCourse(robot.getCourse()+300/COUNT);
+        }
+
+
+            RobotFrame rf = new RobotFrame(robot);
+            rf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            rf.setVisible(true);*/
