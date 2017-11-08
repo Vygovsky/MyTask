9f84cb68be18fba0d771fbfec17ab4cb4412aadf
roman_v <vigmol@ukr.net>
2017-11-08 16:11:27 +0200

    ArrayDemo

---
+++ src/examples/ArraysDemo/ArrayDemo.java
@@ -0,0 +1,32 @@
+package examples.ArraysDemo;
+
+import examples.page8.Robot;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        Robot[] sample = {new Robot(1,1),new Robot(0,0),};
+
+    }
+}
+/*
+        ПРИМЕР №1
+int[]array=new int[10];
+        for (int i = 0; i <array.length ; i++) {
+            System.out.print(array[i]);
+            array[i]=10*(i+1);
+
+        }
+        System.out.println();
+        for (int i = 0; i <array.length ; i++) {
+            System.out.println(array[i]);
+        }*/
+
+
+/*      ПРИМЕР №2
+int[] sample = {12, 56, 7, 34, 89, 43, 23, 9};
+        int sum=0;
+
+        for (int i = 0; i <sample.length ; i++) {
+            sum+=sample[i];
+        }
+        System.out.println(sum);*/
