b2be24d8e7951d09ba3360c4415b7fd2d73e1815
roman_v <vigmol@ukr.net>
2017-11-16 10:36:27 +0200

    task3

---
+++ src/task3/ClassOne.java
@@ -0,0 +1,9 @@
+package task3;
+
+public class ClassOne {
+    public void test(){
+        Classes.StaticTwo inStTow=new Classes.StaticTwo();
+        Classes.StaticThree inStThree=new Classes.StaticThree();
+        Classes.StaticFour inStFour=new Classes.StaticFour();
+    }
+}
