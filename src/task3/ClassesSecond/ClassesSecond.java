53a67b7b38841e189eed47c42bac88eb566aa92a
roman_v <vigmol@ukr.net>
2017-11-16 16:15:15 +0200

    task3_page Classes

---
+++ src/task3/ClassesSecond/ClassesSecond.java
@@ -0,0 +1,9 @@
+package task3.ClassesSecond;
+
+import task3.Classes;
+
+public class ClassesSecond {
+    public void test2() {
+        Classes.StaticFour inStFour = new Classes.StaticFour();
+    }
+}
---
+++ src/task3/ClassesSecond/ThrirdClasses.java
@@ -0,0 +1,10 @@
+package task3.ClassesSecond;
+
+public class ThrirdClasses {
+    public void threeMath(){
+        class ThrirdClas{
+
+        }
+        ThrirdClas tc=new ThrirdClas();
+    }
+}
