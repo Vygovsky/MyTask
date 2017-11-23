2bf9a7295ac3a783ccf67f1bad697fa6b0e28d7d
roman_v <vigmol@ukr.net>
2017-11-23 16:20:54 +0200

    BD

--- src/BD/TestContact.java
+++ src/BD/TestContact.java
@@ -14,20 +14,20 @@ public class TestContact {
         Long cId1 = mc.addContact(c1);
         Long cId2 = mc.addContact(c2);
         Long cId3 = mc.addContact(c3);
-        List<Contact> res1 = mc.getContacts();
+        List<Contact> res1 = mc.findContacts();
         for (Contact con1 : res1) {
             System.out.println(con1);
         }
         System.out.println("UPDATE===============================");
         Contact change = new Contact(cId1, "Михаил", "Троцкий", "097-178-99-48", "miha78@ukr.net");
         mc.updateContact(change);
-        List<Contact> res2 = mc.getContacts();
+        List<Contact> res2 = mc.findContacts();
         for (Contact con2 : res2) {
             System.out.println(con2);
         }
         System.out.println("DELETE===============================");
         mc.deleteContact(cId2);
-        List<Contact> res3 = mc.getContacts();
+        List<Contact> res3 = mc.findContacts();
         for (Contact con3 : res3) {
             System.out.println(con3);
         }
