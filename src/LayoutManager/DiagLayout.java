139fe138766edefea6919b7f4652e3bd4dff765a
roman_v <vigmol@ukr.net>
2017-12-06 16:03:44 +0200

    LayoutManager

--- src/LayoutManager/DiagLayout.java
+++ src/LayoutManager/DiagLayout.java
@@ -3,6 +3,7 @@ package LayoutManager;
 import java.awt.*;
 
 public class DiagLayout implements LayoutManager {
+
     @Override
     public void addLayoutComponent(String name, Component comp) {
 
@@ -15,16 +16,40 @@ public class DiagLayout implements LayoutManager {
 
     @Override
     public Dimension preferredLayoutSize(Container parent) {
-        return null;
+        return computLayoutSize(parent);
     }
 
     @Override
     public Dimension minimumLayoutSize(Container parent) {
-        return null;
+        return computLayoutSize(parent);
+    }
+
+    private Dimension computLayoutSize(Container parent) {
+        int prefWidth = 0;
+        int prefHeight = 0;
+        Component[] component = parent.getComponents();
+        for (int i = 0; i < component.length; i++) {
+            /*prefWidth += component[i].getWidth();
+            prefHeight += component[i].getHeight();*/
+            int width = (int)(component[i].getPreferredSize().getWidth());
+            int height = (int)(component[i].getPreferredSize().getHeight());
+        }
+        return new Dimension(prefWidth, prefHeight);
     }
 
     @Override
     public void layoutContainer(Container parent) {
+        Component[] component = parent.getComponents();
+        int row = 0;
+        int col = 0;
+        int width = parent.getWidth() / component.length;
+        int height = parent.getHeight() / component.length;
+        for (int i = 0; i < component.length; i++) {
+            Rectangle r = new Rectangle(col, row, width, height);
+            component[i].setBounds(r);
 
+            col += width;
+            row += height;
+        }
     }
 }
--- src/LayoutManager/DiagLayoutTest.java
+++ src/LayoutManager/DiagLayoutTest.java
@@ -4,7 +4,19 @@ import javax.swing.*;
 
 public class DiagLayoutTest extends JFrame {
     public DiagLayoutTest() {
-        getContentPane().setLayout();
+        getContentPane().setLayout(new DiagLayout());
+        for (int i = 0; i < 5; i++) {
+            getContentPane().add(new JButton(" " + i));
+        }
+        for (int i = 0; i < 5; i++) {
+            getContentPane().add(new JLabel(" " + i, JLabel.CENTER));
+        }
+        setBounds(100, 100, 400, 300);
     }
 
+    public static void main(String[] args) {
+        DiagLayoutTest dlt = new DiagLayoutTest();
+        dlt.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        dlt.setVisible(true);
+    }
 }
