e97033cb4188242c6efd24149a0025f47dbb3618
Roman_v <vigmol@ukr.net>
2017-12-13 21:16:19 +0200

    Thread ->task2

--- src/Threads/task2/SimpleTaskThread.java
+++ src/Threads/task2/SimpleTaskThread.java
@@ -1,4 +1,26 @@
 package Threads.task2;
 
 public class SimpleTaskThread {
+    public static void  main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            Thread first = new MyThread();
+            first.start();
+
+        }
+    }
+
+    static class  MyThread extends Thread {
+
+        @Override
+        public void run() {
+            try {
+                Long pause = Math.round(Math.random() * 2000);
+                Thread.sleep(pause);
+                System.out.println( "Sipmle Thread - pause " + pause);
+
+            } catch (InterruptedException e) {
+            }
+        }
+    }
 }
