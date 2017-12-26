a413e1ffed154bc4ef1736d73cd8e97c8fd369df
roman_v <vigmol@ukr.net>
2017-12-26 14:14:59 +0200

    deal

--- src/deal/Deal.java
+++ src/deal/Deal.java
@@ -1,5 +1,6 @@
 package deal;
 
+import java.util.Arrays;
 import java.util.Date;
 
 public class Deal {
@@ -36,5 +37,38 @@ public class Deal {
             res = product.getCost();
         }
         return res;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Deal deal = (Deal) o;
+
+        if (date != null ? !date.equals(deal.date) : deal.date != null) return false;
+        if (buyer != null ? !buyer.equals(deal.buyer) : deal.buyer != null) return false;
+        if (seller != null ? !seller.equals(deal.seller) : deal.seller != null) return false;
+        // Probably incorrect - comparing Object[] arrays with Arrays.equals
+        return Arrays.equals(products, deal.products);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = date != null ? date.hashCode() : 0;
+        result = 31 * result + (buyer != null ? buyer.hashCode() : 0);
+        result = 31 * result + (seller != null ? seller.hashCode() : 0);
+        result = 31 * result + Arrays.hashCode(products);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Deal{" +
+                "date=" + date +
+                ", buyer=" + buyer +
+                ", seller=" + seller +
+                ", products=" + Arrays.toString(products) +
+                '}';
     }
 }
--- src/deal/Program.java
+++ src/deal/Program.java
@@ -1,22 +1,43 @@
 package deal;
 
+import java.util.Date;
+
 public class Program {
     public static void main(String[] args) {
+        Date date = new Date();
 
         Deal[] deal = new Deal[5];
-        
+
         Party buyer = new Party();
         buyer.setName("Sergey");
-        
+
         Party seller = new Party();
         seller.setName("Target");
-        
+
         Product[] products = new Product[5];
         products[0] = new Product();
-        products[0].setTitle("Maslo");
+
+        products[0].setTitle("масло Олейна");
         products[0].setQuantity(5);
-        products[0].setPrice(80.6);
-        
+        products[0].setPrice(20);
+        double res = products[0].getCost();
+        System.out.println("Общая стоимость: " + res + " " + date);
+
+        products[0].setTitle("пиво Опелля");
+        products[0].setQuantity(2);
+        products[0].setPrice(14.56);
+        double res1 = products[0].getCost();
+        System.out.println("Общая стоимость: " + res1 + " " + date);
+
+        products[0].setTitle("шоколад Аленка");
+        products[0].setQuantity(3);
+        products[0].setPrice(18.20);
+        double res2 = products[0].getCost();
+        System.out.println("Общая стоимость: " + res2 + " " + date);
+
+
         deal[0] = new Deal(buyer, seller, products);
+
+
     }
 }
