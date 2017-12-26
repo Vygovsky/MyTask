9b46601c69feffa24c5dc14400266679c6d5c977
Petrykin Sergey <serhii.petrykin@gmail.com>
2017-12-26 13:49:22 +0200

    Update Program.java

--- src/deal/Program.java
+++ src/deal/Program.java
@@ -4,11 +4,19 @@ public class Program {
     public static void main(String[] args) {
 
         Deal[] deal = new Deal[5];
-        Party party = new Party();
-        Product product = new Product();
-        deal[0] = new Deal(new Party(party.setName("Sergey"), new Party(party.setName("Target"),
-                product.setTitle("Maslo"), product.setQuantity(5), product.setPrice(80,6));
-
-
+        
+        Party buyer = new Party();
+        buyer.setName("Sergey");
+        
+        Party seller = new Party();
+        seller.setName("Target");
+        
+        Product[] products = new Product[5];
+        products[0] = new Product();
+        products[0].setTitle("Maslo");
+        products[0].setQuantity(5);
+        products[0].setPrice(80.6);
+        
+        deal[0] = new Deal(buyer, seller, products);
     }
 }
