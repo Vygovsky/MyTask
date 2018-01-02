b5ed4cf36708377c637d168af5852fa58f9fadc0
Petrykin Sergey <serhii.petrykin@gmail.com>
2018-01-02 15:29:00 +0200

    Update DomExample.java
    some fixes

--- src/Xml/DomExample.java
+++ src/Xml/DomExample.java
@@ -21,18 +21,19 @@ public class DomExample {
 
             Node root = document.getDocumentElement();
             System.out.println("List of books");
-            System.out.println();
+            System.out.println("*************************");
             NodeList books = root.getChildNodes();
             for (int i = 0; i < books.getLength(); i++) {
                 Node book = books.item(i);
                 if (book.getNodeType() != Node.TEXT_NODE) {
                     NodeList bookProps = book.getChildNodes();
                     for (int j = 0; j < bookProps.getLength(); j++) {
-                        Node bookProp = bookProps.item(i);
+                        Node bookProp = bookProps.item(j);
                         if (bookProp.getNodeType() != Node.TEXT_NODE) {
-                            System.out.println(bookProp.getNodeName() + ":" + bookProp.getChildNodes().item(i).getTextContent());
+                            System.out.println(bookProp.getNodeName() + ":" + bookProp.getTextContent());
                         }
                     }
+                    System.out.println("*************************");
                 }
             }
         } catch (ParserConfigurationException e) {
