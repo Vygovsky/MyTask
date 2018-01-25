42e79556c3008947d4786ca5bf70a1ebb95e7612
Roman_v <vigmol@ukr.net>
2018-01-25 21:20:49 +0200

    Test

---
+++ src/Test/Test2.java
@@ -0,0 +1,21 @@
+package Test;
+
+import java.util.Scanner;
+
+/**
+ * Created by Roman_v on 03.01.2018.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================================");
+        for (int i = 0; i < 3; i++) {
+            String s1 = sc.next();
+            int x = sc.nextInt();
+            System.out.printf("%-10s%03d%n",s1,x);
+        }
+        System.out.println("================================");
+
+    }
+}
+
---
+++ src/Test/Test3.java
@@ -0,0 +1,27 @@
+package Test;
+
+import java.util.Scanner;
+
+/**
+ * Created by Roman_v on 05.01.2018.
+ */
+public class Test3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int i = 1;
+        int res = 0;
+
+        while (i <= 10) {
+            res = N * i;
+            System.out.printf("%d x %d = %d%n", N, i, res);
+            i++;
+
+        }
+
+    }
+}
+
+/*int res=2;
+        res*=N;
+        System.out.println(res);*/
