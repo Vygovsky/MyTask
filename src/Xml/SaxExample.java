54067733f8f5413b14cff24f6397521a8eef7ccf
Roman_v <vigmol@ukr.net>
2018-01-31 21:41:37 +0200

    XML

--- src/Xml/SaxExample.java
+++ src/Xml/SaxExample.java
@@ -12,22 +12,21 @@ import javax.xml.parsers.SAXParserFactory;
 public class SaxExample {
     public static void main(String[] args) {
         final String fileName = "Phonebook.xml";
-
-        SAXParserFactory factory = SAXParserFactory.newInstance();
         try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
             SAXParser saxParser = factory.newSAXParser();
 
             DefaultHandler handler = new DefaultHandler() {
                 boolean name = false;
 
                 public void startElement(String url, String localName, String qName, Attributes attributes) throws SAXException {
-                    if (qName.equalsIgnoreCase("NAME")) ;
+                    if (qName.equalsIgnoreCase("NAME")){
                     name = true;
                 }
-
+            }
                 public void characters(char ch[], int start, int length) throws SAXException {
                     if (name) {
-                        System.out.println("Name " + new String(ch, start, length));
+                        System.out.println("Name: " + new String(ch, start, length));
                     }
                 }
             };
