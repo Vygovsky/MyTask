8112a10c9371c9c62d1bbe3a40088140a19bd1fd
roman_v <vigmol@ukr.net>
2018-01-31 16:01:32 +0200

    test5

--- src/Test/Test5.java
+++ src/Test/Test5.java
@@ -13,10 +13,11 @@ public class Test5 {
             try {
                 long x = sc.nextInt();
                 System.out.println(x + " can be fitted in:");
-                if (x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE) System.out.println(" * byte");
-                if (x >= Short.MIN_VALUE && x <= Short.MAX_VALUE) System.out.println(" * short");
-                if (x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE) System.out.println(" * int");
-                if (x >= Long.MIN_VALUE && x <= Long.MAX_VALUE) System.out.println(" * long");
+                if (x >= -128 && x <= 127) System.out.println(" * byte");
+                if (x >= (short) -Math.pow(2, 15) && x <= (short) Math.pow(2, 15) - 1) System.out.println(" * short");
+                if (x >= (int) -Math.pow(2, 31) && x <= (int) Math.pow(2, 31) - 1) System.out.println(" * int");
+                if (x >= (long) -Math.pow(2, 63) && x <= (long) Math.pow(2, 63) - 1) System.out.println(" * long");
+
             } catch (Exception e) {
                 System.out.println(sc.next() + " can't be fitted anywhere.");
             }
@@ -25,3 +26,7 @@ public class Test5 {
         }
     }
 }
+/* if (x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE) System.out.println(" * byte");
+                if (x >= Short.MIN_VALUE && x <= Short.MAX_VALUE) System.out.println(" * short");
+                if (x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE) System.out.println(" * int");
+                if (x >= Long.MIN_VALUE && x <= Long.MAX_VALUE) System.out.println(" * long");*/
