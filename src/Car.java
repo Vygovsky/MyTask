1d890624ac0fabf9fc4cce1c74160115f89313ad
Roman_v <vigmol@ukr.net>
2018-02-23 23:49:40 +0200

    xo add TestCar

---
+++ src/Car.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Roman_v on 14.02.2018.
+ */
+public class Car {
+    int speed;
+
+    public void showSpeed(){
+        System.out.println(this.speed);
+    }
+    public void setSpeed(int newSpeed){
+        this.speed=newSpeed;
+        showSpeed();
+        newSpeed=8;
+        speed=3;
+        setSetSpeed(newSpeed);
+        showSpeed();
+    }
+
+    public void setSetSpeed(int speed) {
+        showSpeed();
+        speed=speed;
+        this.speed=speed;
+        showSpeed();
+    }
+}
