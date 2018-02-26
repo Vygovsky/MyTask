becabee308219b30cfcfc3cac55d6d371dbff179
Roman_v <vigmol@ukr.net>
2018-02-26 21:17:15 +0200

    test3 AlternateSum

---
+++ src/task3/AlternateSqSum.java
@@ -0,0 +1,24 @@
+package task3;
+
+/**
+ * Created by Roman_v on 24.02.2018.
+ */
+public class AlternateSqSum {
+    public static int[] alternateSqSum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                arr[i] *= arr[i];
+                sum += arr[i];
+            } else {
+                sum += arr[i];
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        AlternateSqSum.alternateSqSum(new int[]{11, 12, 13, 14, 15});
+
+    }
+}
