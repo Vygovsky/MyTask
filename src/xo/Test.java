113a406ed65626a8d723cd82537cbf1f107f7144
Roman_v <vigmol@ukr.net>
2018-02-28 20:12:20 +0200

    xo fix

--- src/xo/Test.java
+++ src/xo/Test.java
@@ -5,25 +5,24 @@ package xo;
  */
 public class Test {
     public static void main(String[] args) {
-        Game game=new Game();
+        Board board = new Board();
+        Game game = new Game();
         game.printGame();
-        TwoPlayersGame game1=new TwoPlayersGame("Roman","Max");
+        TwoPlayersGame game1 = new TwoPlayersGame("Roman", "Max");
         game1.printOnePlayerName();
         game1.printTwoPlayerName();
         game1.printGame();
 
-        ThreePlayersGame game2=new ThreePlayersGame("Roman","Max","Mike");
+        ThreePlayersGame game2 = new ThreePlayersGame("Roman", "Max", "Mike");
 
         game2.printOnePlayerName();
         game2.printTwoPlayerName();
         game2.printThreePlayersGame();
         game2.printGame();
-
-
+        printGameAndBoard(board, game);
     }
 
-    private static void printGameAndBoard(Board board,Game game) {
-
+    private static void printGameAndBoard(Board board, Game game) {
         game.printGame();
         board.printBoard();
     }
