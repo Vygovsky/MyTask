d7ada66fc4c8eeb5bf1a5d878d85bc38aa2bf436
Roman_v <vigmol@ukr.net>
2018-03-21 22:45:49 +0200

    dobroe lesson2

---
+++ src/dobroe/lesson2/Factorial2.java
@@ -0,0 +1,25 @@
+package dobroe.lesson2;
+
+
+public class Factorial2 {
+    public static void main(String[] args) {
+        System.out.println(findFactorial(-3));
+    }
+
+    public static int findFactorial(int number) {
+        int resultFactorial = 0;
+
+        negativeFactorial(number);
+
+        for (int i = 2; i <= number; i++) {
+            resultFactorial *= i;
+        }
+        return resultFactorial;
+    }
+
+    public static void negativeFactorial(int number) {
+        if (number < 0) {
+            System.out.println("number < 0 не может быть");
+        }
+    }
+}
