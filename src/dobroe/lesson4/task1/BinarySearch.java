b0374e01ba5fe182831debea5193da84cb52bbd5
Roman_v <vigmol@ukr.net>
2018-05-14 23:22:56 +0300

    binarySer_fix

--- src/dobroe/lesson4/task1/BinarySearch.java
+++ src/dobroe/lesson4/task1/BinarySearch.java
@@ -5,31 +5,24 @@ public class BinarySearch {
     public static int binarySearch(int[] data, int element) {
         int left = 0;
         int right = data.length;
-
-        while (true)
-        {
-            int mid = left + (right - left) / 2;
-
-            if (data[mid] == element)
-                return mid;
-
-            if (data[mid] > element)
-                right = mid;
-            else
+        int mid = left + (right - left) / 2;
+        while (data[mid] != element) {
+            // return mid;
+            if (data[mid] > element) {
+                right = mid - 1;
+            } else
                 left = mid + 1;
+            mid = (left + right) / 2;
         }
+        if (left <= right) {
+            return mid;
+        }
+        return -1;
     }
 
 
     public static void main(String[] args) {
-        int[] array = new int[]{-2, 3, 5, 8, 6, 12, 13};
-        System.out.println(binarySearch(array, 6));
+        int[] array = new int[]{-2, 3, 5, 8, 9, 12, 13};
+        System.out.println(binarySearch(array, 1));
     }
 }
-/* for (int i = 0; i < data.length; i++) {
-
-                if (element == data[i]) {
-                    return i;
-                }
-            }
-        return -1;*/
